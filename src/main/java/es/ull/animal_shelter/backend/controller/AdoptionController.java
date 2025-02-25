221d76d7d866bddda7cef1fd07aec48640e15341
pau-regalado <alu0101330174@ull.edu.es>
2025-02-25 11:29:48 +0000

    Create class adoption controller and service

--- src/main/java/es/ull/animal_shelter/backend/controller/AdoptionController.java
+++ src/main/java/es/ull/animal_shelter/backend/controller/AdoptionController.java
@@ -1,30 +1,35 @@
 package es.ull.animal_shelter.backend.controller;
 
-
 import es.ull.animal_shelter.backend.model.Adoption;
-import es.ull.animal_shelter.backend.model.Animal;
-import es.ull.animal_shelter.backend.service.AnimalService;
+import es.ull.animal_shelter.backend.service.AdoptionService;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/adoptions")
 public class AdoptionController {
 
-    //@Autowired
-    //private AdoptionService animalService;
+    @Autowired
+    private AdoptionService adoptionService;
 
     @PostMapping
     public Adoption save(@RequestBody Adoption adoption) {
         LogManager.getLogger(this.getClass()).warn(adoption.toString());
-        //adoptionService.save(adoption);
+        adoptionService.save(adoption);
         return adoption;
+    }
+
+    @GetMapping
+    public List<Adoption> findAll() {
+        return adoptionService.findAll();
     }
 
     @GetMapping("/{id}")
     public Adoption findById(@PathVariable String id) {
-        //return adoptionService.findById(id);
+        return adoptionService.findById(id);
     }
 
 }
--- src/main/java/es/ull/animal_shelter/backend/service/AdoptionService.java
+++ src/main/java/es/ull/animal_shelter/backend/service/AdoptionService.java
@@ -1,4 +1,31 @@
 package es.ull.animal_shelter.backend.service;
 
+import es.ull.animal_shelter.backend.model.Adoption;
+import es.ull.animal_shelter.backend.repository.AdoptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class AdoptionService {
+    @Autowired
+    private AdoptionRepository adoptionRepository;
+
+    public Adoption save(Adoption adoption) {
+        adoption.setId(UUID.randomUUID().toString());
+        return adoptionRepository.save(adoption);
+    }
+
+    public List<Adoption> findAll() {
+        return adoptionRepository.findAll();
+    }
+
+    public Adoption findById(String id) {
+        return adoptionRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Adoption no encontrado con ID: " + id));
+    }
 }
